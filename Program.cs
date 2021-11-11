ed210aa7782b927ecbd91bb168eec96e5789a7ec
Rachapudivjy <rachapudivijay27@gmail.com>
2021-11-11 11:18:42 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Properities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
